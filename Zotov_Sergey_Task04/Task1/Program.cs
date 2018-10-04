dd3eb3e9f09c462ebf1a4432aafb36c9d37ed5dd
balanced2133 <zotov12.1998@gmail.com>
2018-10-04 14:53:25 +0300

    Added Task04: Task1 works and doesn't completed Task2

---
+++ Zotov_Sergey_Task04/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string myString = Console.ReadLine();
+            Console.WriteLine($"Средняя длина слова в введенной строке равна {Math.Round(GetAverageLengthOfWords(myString), 2)}");
+            Console.ReadKey();
+        }
+
+        static double GetAverageLengthOfWords(string myString)
+        {
+            string[] tempString = myString.Split(new char[] { '.', ',', '!', '?', ';',
+                ':', '-', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            double avg = 0;
+            int index = 0;
+
+            foreach (var v in tempString)
+            {
+                avg = avg + v.Length;
+                index++;
+            }
+
+            return avg / index;
+        }
+    }
+}
---
+++ Zotov_Sergey_Task04/Task2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Does not work
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resultLine = string.Empty;
+            Console.WriteLine("Введите первую строку:");
+            string firstLine = Console.ReadLine();
+            Console.WriteLine("Введите вторую строку:");
+            string secondLine = Console.ReadLine();
+            Console.WriteLine($"Результирующая строка:\n{GetNewString(firstLine, secondLine)}");
+            Console.ReadKey();
+
+        }
+
+        static string GetNewString(string firstLine, string secondLine)
+        {
+            string resultLine = string.Empty;
+            List<char> charArr2 = GetCharArray(firstLine);
+            int index = 0;
+            for (int i = 0; i < firstLine.Length; ++i)
+            {
+                resultLine = resultLine +  firstLine[i];
+
+                foreach(var ch in charArr2)
+                {
+                    if (resultLine[index] == ch)
+                    {
+                        resultLine = resultLine + ch;
+                        index++;
+                        break;
+                    }
+                    /*if (resultLine[i] == ch)
+                    {
+                        resultLine = resultLine + ch;
+                        break;
+                    }
+                    else continue;*/
+                }
+            }
+
+
+            return resultLine;
+        }
+
+        static List<char> GetCharArray(string firstLine)
+        {
+            List<char> charArr = new List<char>();
+            foreach(char ch in firstLine)
+            {
+                if (ch != ' ')
+                {
+                    charArr.Add(ch);
+                }
+            }
+            return charArr;
+        }
+    }
+}
