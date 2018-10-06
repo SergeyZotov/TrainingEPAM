5f930b7984b9435b27e4d8d1d8fe85681f85d59d
balanced2133 <zotov12.1998@gmail.com>
2018-10-06 20:26:44 +0300

    Changed Task4

---
+++ Zotov_Sergey_Task04/Task4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "";
+            StringBuilder sb = new StringBuilder();
+            int N = 100;
+            var watcher1 = Stopwatch.StartNew();
+
+            for (int i = 0; i < N; i++)
+            {
+                str += "*";
+            }
+            watcher1.Stop();
+
+            Console.WriteLine($"Вот так работает сложение в string:\t\t{watcher1.Elapsed}");
+
+            var watcher = Stopwatch.StartNew();
+
+            watcher.Start();
+            for (int i = 0; i < N; i++)
+            {
+                sb.Append("*");
+            }
+            watcher.Stop();
+
+            Console.WriteLine($"Вот так работает сложение в StringBuilder:\t{watcher.Elapsed}");
+            Console.ReadKey();
+        }
+    }
+}
