a38763ee0d330707bd87313692efb9d0f81337c1
balanced2133 <zotov12.1998@gmail.com>
2018-10-11 12:47:28 +0300

    Added Taks4: added operator +

---
+++ Zotov_Sergey_Task05/Task4/MyString.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class MyString
+    {
+        private char[] newCharArr;
+
+        public MyString(string str)
+        {
+            int index = 0;
+            newCharArr = new char[str.Length];
+
+            while(index < str.Length)
+            {
+                newCharArr[index] = str[index];
+                index++;
+            }
+        }
+
+        public static MyString operator +(MyString string1, MyString string2)
+        {
+            int firstStringLength = string1.Length;
+
+            string resultString = string1.ToString();
+
+            for (int i = 0; i < string2.Length; ++i)
+            {
+                resultString += string2[i];
+            }
+
+            return new MyString(resultString);
+        }
+
+        public static MyString operator -(MyString srcString, MyString substring)
+        {
+            int index = 0;
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < srcString.Length; ++i)
+            {
+
+            }
+
+            return new MyString(sb.ToString());
+        }
+
+        public static bool operator ==(MyString string1, MyString string2)
+        {
+            if (string1.ToString() == string2.ToString())
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator !=(MyString string1, MyString string2)
+        {
+            if (string1.ToString() == string2.ToString())
+                return false;
+            else
+                return true;
+        }
+
+        public override string ToString()
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach(char value in newCharArr)
+            {
+                sb.Append(value);
+            }
+
+            return sb.ToString();
+        }
+
+        public char this[int index]
+        {
+            get
+            {
+                if (index >-1 && index < newCharArr.Length)
+                    return newCharArr[index];
+                else throw new ArgumentOutOfRangeException();
+            }
+
+            set
+            {
+                if (index > newCharArr.Length)
+                    throw new ArgumentOutOfRangeException();
+
+                newCharArr[index] = value;
+            }
+            
+        }
+
+        internal int Length { get => newCharArr.Length; }
+    }    
+}
---
+++ Zotov_Sergey_Task05/Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите вашу строку");
+            MyString ms1 = new MyString(" hello");
+            MyString ms2 = new MyString("hello");
+            Console.WriteLine(ms2 + ms1);
+
+            /*for (int i = 0; i < ms2.Length; ++i)
+            {
+                Console.Write(ms2[i].ToString());
+            }*/
+
+            Console.ReadKey();
+        }
+    }
+}
