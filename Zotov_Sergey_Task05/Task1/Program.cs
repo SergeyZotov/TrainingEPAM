166d676305f5a36e07b355794ddaeee281973c6a
balanced2133 <zotov12.1998@gmail.com>
2018-10-13 14:56:12 +0300

    Upgraded some code in Task1 and Task2

--- Zotov_Sergey_Task05/Task1/Program.cs
+++ Zotov_Sergey_Task05/Task1/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Globalization;
-// Testing
+
 namespace Task1
 {
     class Program
--- Zotov_Sergey_Task05/Task2/Round.cs
+++ Zotov_Sergey_Task05/Task2/Round.cs
@@ -5,31 +5,34 @@ namespace Task2
     public class Round
     {
         private protected double radius;
+        private protected double x;
+        private protected double y;
 
         public Round(string x, string y, string radius)
         {
-            try
+            if (!double.TryParse(x, out this.x) || !double.TryParse(x, out this.x) ||
+                !double.TryParse(x, out this.x))
             {
-                Y = double.Parse(y);
-                X = double.Parse(x);
-                Radius = double.Parse(radius);
+                throw new FormatException("Coordinates and radius must be positive numbers");
             }
-            catch (FormatException)
+            else
             {
-                throw new FormatException("Координаты и радиус должны быть целым или вещественным числом");
+                X = this.x;
+                Y = this.y;
+                Radius = this.radius;
             }
         }
 
-        internal protected double X { private set; get; }
+        internal protected double X { private protected set; get; }
 
-        internal protected double Y { private set; get; }
+        internal protected double Y { private protected set; get; }
 
         internal protected double Radius
         {
             private protected set
             {
                 if (value < 0)
-                    throw new ArgumentException("Радиус не может быть меньше нуля");
+                    throw new ArgumentException("Radius cannot be less than 0");
 
                 radius = value;
             }
