56291b1bad7171823dbc121c97093ff63d4bb984
balanced2133 <zotov12.1998@gmail.com>
2018-10-13 17:44:20 +0300

    Added Task07: Task02

---
+++ Zotov_Sergey_Task07/Task2/GeometricProgression.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class GeometricProgression : ISeries
+    {
+        private double denominatorOfProgression;
+        private int currentIndex;
+        private double start;
+        private int count;
+
+        public GeometricProgression(string start, string denominatorOfProgression, string count)
+        {
+            if (!double.TryParse(start, out this.start) || 
+                !double.TryParse(denominatorOfProgression, out this.denominatorOfProgression))
+            {
+                throw new ArgumentException("The first member and denominator of progression " +
+                    "cannot be equal to 0");
+            }
+
+            if (!int.TryParse(count, out this.count) || this.count < 1)
+            {
+                throw new ArgumentException("The count of your sequence cannot be less than 1 and must be integer!");
+            }
+
+            Start = this.start;
+            DenominatorOfProgression = this.denominatorOfProgression;
+            Count = this.count;
+        }
+
+        internal protected int Count
+        {
+            private set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Count of your sequence cannot be less than 1 ");
+
+                count = value;
+            }
+
+            get => count;
+        }
+
+        internal protected double Start
+        {
+            private set
+            {
+                if (value == 0)
+                    throw new ArgumentException("The first member of sequence cannot be equals to 0!");
+
+                start = value;
+            }
+
+            get => start;
+        }
+
+        internal protected double DenominatorOfProgression
+        {
+            private set
+            {
+                if (value == 0)
+                    throw new ArgumentException("The denominator of progression of sequence cannot be equals to 0!");
+                denominatorOfProgression = value;
+            }
+
+            get => denominatorOfProgression;
+        }
+
+        public double GetCurrent()
+        {
+            return start * Math.Pow(denominatorOfProgression, currentIndex - 1);
+        }
+
+        public bool MoveNext()
+        {
+            currentIndex++;
+            return true;
+        }
+
+        public void Reset()
+        {
+            currentIndex = 2;
+        }
+
+
+    }
+}
---
+++ Zotov_Sergey_Task07/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first member, denominator " +
+                "and number of your progression:");
+            GeometricProgression geometricProgression = new GeometricProgression(Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
+            PrintSeries(geometricProgression, geometricProgression.Count);
+            Console.ReadKey();
+        }
+
+        static void PrintSeries(ISeries mySeries, int count)
+        {
+            while (mySeries.MoveNext() && count != 0)
+            {
+                Console.Write($"{mySeries.GetCurrent()}  ");
+                count--;
+            }
+        }
+    }
+}
