ec720c0f94ffd4dae5a6b96126346bf644461214
balanced2133 <zotov12.1998@gmail.com>
2018-12-06 12:04:50 +0300

    Added some missing files

---
+++ UsersAndAwards/UsersAndAwards.BLL/WebLogic.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsersAndAwards.DAL;
+
+namespace UsersAndAwards.BLL
+{
+    public class WebLogic : IStorage
+    {
+        IStorage storage;
+
+        public Logic()
+        {
+            if (DBStorage.providerName.Contains("System.Data.SqlClient"))
+            {
+                storage = new DBStorage();
+            }
+            else
+            {
+                storage = new InMemoryStorage();
+            }
+        }
+
+        public List<UserViewModel> GetUsersForUI()
+        {
+            return storage.GetAllUsers().Select(u => new UserViewModel(u)).ToList();
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return storage.GetAllUsers().ToList();
+        }
+
+        public List<Award> GetAllAwards()
+        {
+            return storage.GetAllAwards();
+        }
+
+        public void AddUser(User newUser)
+        {
+            storage.AddUser(newUser);
+        }
+
+        public void AddAward(Award award)
+        {
+            storage.AddAward(award);
+        }
+
+        public void EditAward(Award award, int indexOfSelectedAward)
+        {
+            storage.EditAward(award, indexOfSelectedAward);
+        }
+
+        public bool RemoveAward(Award award)
+        {
+            return storage.RemoveAward(award);
+        }
+
+        public void EditUser(User newUser, int indexOfSelectedUser)
+        {
+            storage.EditUser(newUser, indexOfSelectedUser);
+        }
+
+        public bool RemoveUser(User user)
+        {
+            return storage.RemoveUser(user);
+        }
+    }
+}
---
+++ UsersAndAwards/UsersAndAwards.PL/Web/UsersAndAwards.PL.Web/Models/UserAndAwardsModel.cs
@@ -0,0 +1,26 @@
+﻿using Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UsersAndAwards.PL.Web.Models
+{
+    public class UserAndAwardsModel
+    {
+        public UserViewModel User { get; set; }
+        public List<AwardViewModel> AllAvailableAwards { get; set; }
+
+        public User ToUser(UserViewModel user, List<AwardViewModel> aw)
+        {
+            return new User(user.FirstName, user.LastName, user.Birthdate)
+            {
+                Id = user.Id,
+                Awards = aw
+                .Where(a => a.IsAssigned == true)
+                .Select(a => new Award(a.Title, a.Description)
+                {
+                    AwardId = a.Id
+                }).ToList()
+            };
+        }
+    }
+}
